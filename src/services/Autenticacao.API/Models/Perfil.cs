e0eec49a72c390904f64a5db5598499e54292093
Luiz Fernando <luizfernando_rodrigues12@hotmail.com>
2021-06-29 02:03:39 -0300

    Inicio da modelagem do serviço de identidade

---
+++ src/services/Autenticacao.API/Models/Perfil.cs
@@ -0,0 +1,9 @@
+﻿namespace Autenticacao.API.Models
+{
+    public class Perfil
+    {
+        public long? Id { get; set; }
+        public string Descricao { get; set; }
+        public int Tipo { get; set; }
+    }
+}
